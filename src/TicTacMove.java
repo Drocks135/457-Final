f23df84b702e57932c0eb15f24c38c42b6c7e3af
Drocks135 <claybaud@mail.gvsu.edu>
2020-12-04 19:58:44 -0500

    Added a string based constructor to parse inside the method

--- src/TicTacMove.java
+++ src/TicTacMove.java
@@ -1,3 +1,5 @@
+import java.util.StringTokenizer;
+
 public class TicTacMove {
     private boolean player;
     private int move;
@@ -11,6 +13,19 @@ public class TicTacMove {
     public TicTacMove(boolean player, int move){
         this.player = player;
         this.move = move;
+    }
+
+    /******************************************************************
+     * Creates a tictac move that parses two strings into a boolean
+     * and an integer
+     * @param player: A string that represents a boolean of which player the
+     *              move is for, 0 represents O, 1 represents X
+     * @param move: This string represents an integer of which spot on
+     *            the board to make the move on
+     ******************************************************************/
+    public TicTacMove(String player, String move){
+        this.player = Boolean.parseBoolean(player);
+        this.move = Integer.parseInt(move);
     }
     /******************************************************************
      * Returns and integer representing which spot on the board a move
