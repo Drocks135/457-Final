06d4aae3085ee5536b4197a8b70658a176fc7bcf
Drocks135 <claybaud@mail.gvsu.edu>
2020-12-05 22:10:30 -0500

    This probs breaks smile

--- src/Main.java
+++ src/Main.java
@@ -2,6 +2,12 @@ import javax.swing.SwingUtilities;
 
 public class Main {
     public static void main(String[] args){
+        TicTacServer server = new TicTacServer();
+        TicTacServerHandler serverHandler;
+        TicTacLogic logic = new TicTacLogic(3, true);
+        TicTacBoard board = new TicTacBoard();
+
+        serverHandler = new TicTacServerHandler(board, server, logic, 1233);
         SwingUtilities.invokeLater(new Runnable(){
             @Override
             public void run() {
