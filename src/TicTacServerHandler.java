82aa1ae806ab2976267c9fdd80162b4651591727
Drocks135 <claybaud@mail.gvsu.edu>
2020-12-05 20:18:38 -0500

    Start of server handeling code

--- src/TicTacServerHandler.java
+++ src/TicTacServerHandler.java
@@ -1,4 +1,20 @@
-package PACKAGE_NAME;
+public class TicTacServerHandler {
+    private TicTacBoard board;
+    private TicTacServer server;
+    private TicTacLogic game;
 
-public class TicTacHandler {
+    public TicTacServerHandler(TicTacBoard board, TicTacServer server, TicTacLogic game, int port){
+        this.board = board;
+        this.server = server;
+        this.game = game;
+        StartServer(port);
+    }
+
+    private void StartServer(int port){
+        server.StartServer(port);
+    }
+
+    private void MakeMove(TicTacMove move){
+
+    }
 }
