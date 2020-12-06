4876edd57cc648a396f01d82133de42eaf30c657
PerpetualLink <Samack1011@gmail.com>
2020-12-05 22:13:12 -0500

    New Game Logic because we couldn't use static function calls

--- src/TicTacLogic.java
+++ src/TicTacLogic.java
@@ -1,35 +1,138 @@
+import javax.crypto.Cipher;
 import java.util.Scanner;
 public class TicTacLogic {
     private int[][] GameBoard;
     private boolean CurrentPlayer;
+    private int boardSize;
 
     public TicTacLogic(int boardSize, Boolean startingPlayer){
+        this.boardSize = boardSize;
         GameBoard = new int[boardSize][boardSize];
         CurrentPlayer = startingPlayer;
+        ResetBoard();
     }
 
     public void MakeMove(TicTacMove move){
-        GameBoard[move.GetRow()][move.GetCol()] =  move.GetPlayer() ? 1 : 0;
+        if (isValidMove(move.GetRow(),move.GetCol(), move.GetPlayer())) {
+            GameBoard[move.GetRow()][move.GetCol()] = move.GetPlayer() ? 1 : 0;
+            SwitchPlayer();
+        }
     }
 
+    /** This Method Resets the board. We loop through our size and
+     *  Initialize all the values to -1 telling us that no one has
+     *  moved on this location.*/
     public void ResetBoard(){
-
+        for(int i = 0; i < boardSize; i ++){
+            for(int j = 0; j < boardSize; j++){
+                GameBoard[i][j] = -1;
+            }
+        }
     }
 
-    //0 for O win, 1 for X win, 2 for draw
+    /** This Method Determines whether the game has been won. **/
     public int HasWon(){
-        return 0;
+        int countX = 0;
+        int countO = 0;
+        boolean canContinue = false;
+
+        // Rows
+        for (int i = 0; i < boardSize; i++) {
+            for (int j = 0; j < boardSize; j++){
+                if (GameBoard[i][j] == 1){
+                    countX ++;
+                }
+                else if (GameBoard[i][j] == 0){
+                    countO ++;
+                }
+            }
+            if (countX == boardSize) return 1;
+            if (countO == boardSize) return 0;
+            countO = 0;
+            countX = 0;
+        }
+        // Cols
+        for (int i = 0; i < boardSize; i++) {
+            for (int j = 0; j < boardSize; j++){
+                if (GameBoard[j][i] == 1){
+                    countX ++;
+                }
+                else if (GameBoard[j][i] == 0){
+                    countO ++;
+                }
+            }
+            if (countX == boardSize) return 1;
+            if (countO == boardSize) return 0;
+            countO = 0;
+            countX = 0;
+        }
+        // Diagonals top top left to bottom right
+        for (int i = 0; i < boardSize; i++){
+            if (GameBoard[i][i] == 1){
+                countX ++;
+            }
+            if (GameBoard[i][i] == 0){
+                countO ++;
+            }
+            if (countX == boardSize) return 1;
+            if (countO == boardSize) return 0;
+        }
+        countO = 0;
+        countX = 0;
+        // Diagonals bottom left to top right
+        for (int i = boardSize - 1; i > -1; i--){
+            if (GameBoard[i][i] == 1){
+                countX ++;
+            }
+            if (GameBoard[i][i] == 0){
+                countO ++;
+            }
+            if (countX == boardSize) return 1;
+            if (countO == boardSize) return 0;
+        }
+
+        // Draw
+        int i = 0;
+        int j = 0;
+        while(!canContinue){
+            // If empty space we can continue.
+            if (GameBoard[i][j] == -1){
+                canContinue = true;
+            }
+            // If we checked the whole board
+            if (i == boardSize -1 && j == boardSize -1){
+                // See if you still can't continue. If so return false.
+                if (!canContinue){
+                    return 2;
+                }
+            }
+            // If J is end of row increment i reset j to 0;
+            if (j == boardSize - 1){
+                i++;
+                j = -1;
+            }
+            // Increment j;
+            j++;
+        }
+
+        // Game is not won.
+        return -1;
     }
 
     private void SwitchPlayer(){
-
+        if (CurrentPlayer) { CurrentPlayer = false; }
+        else { CurrentPlayer = true; }
     }
 
     public Boolean GetCurrentPlayer(){
         return CurrentPlayer;
     }
 
-    public Boolean CanPlay(Boolean player){
-        return true;
+    public Boolean isValidMove(int row, int col, boolean player){
+        if (player == CurrentPlayer){
+            return (GameBoard[row][col] != -1);
+        }
+        return false;
     }
+
 }
