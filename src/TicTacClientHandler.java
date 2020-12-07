d5b082955d6667df0131ea510fa5575edc2487fd
Drocks135 <claybaud@mail.gvsu.edu>
2020-12-07 18:53:08 -0500

    Fixed startup bug

--- src/TicTacClientHandler.java
+++ src/TicTacClientHandler.java
@@ -12,10 +12,9 @@ public class TicTacClientHandler implements TicTacHandler{
      *****************************************************************/
     public TicTacClientHandler(String hostName, int hostPort) throws Exception{
         this.client = new TicTacClient();
+        client.StartClient(hostPort, hostName, this);
         this.game = new TicTacLogic(3, true);
         this.board = new TicTacBoard(this);
-        client.StartClient(hostPort, hostName, this);
-
     }
 
     /*****************************************************************
